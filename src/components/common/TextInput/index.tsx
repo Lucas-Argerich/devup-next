0dc6caa261b6d019df4739b636ccd9b6d0a3b4b5
Lucas Argerich <lucasargerichcoder@gmail.com>
2023-06-13 16:04:49 -0300

    FEAT: Text Input Conditional Error Display

--- src/components/common/TextInput/index.tsx
+++ src/components/common/TextInput/index.tsx
@@ -1,22 +1,33 @@
 'use client';
 
-import React, { useId, useRef, useState } from 'react';
+import React, { useEffect, useId, useRef, useState } from 'react';
 import styles from './index.module.css';
+import { WarnIcon } from '@/components/icons/Icons';
 
 interface ITextInput {
   label: string;
   type?: 'text' | 'email' | 'password';
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  error?: TextInputError;
 }
+
+export type TextInputError = {
+  condition: (
+    value: string | ReadonlyArray<string> | number | undefined
+  ) => boolean;
+  message: string;
+};
 
 export default function TextInput({
   label,
   type = 'text',
   onChange,
+  error,
 }: ITextInput) {
   const id = useId();
   const inputRef = useRef<HTMLInputElement>(null);
   const [active, setActive] = useState(false);
+  const [hasError, setHasError] = useState(false);
 
   const handleClick = () => {
     inputRef.current?.focus();
@@ -27,21 +38,36 @@ export default function TextInput({
     e.target.value.length === 0 ? setActive(false) : setActive(true);
   };
 
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement, Element>) => {
+    error && setHasError(!error.condition(e.target.value));
+  };
+
   return (
-    <div
-      className={`${styles.div} ${active ? styles.active : ''}`}
-      onClick={handleClick}
-    >
-      <label className={styles.label} htmlFor={id}>
-        {label}
-      </label>
-      <input
-        className={styles.input}
-        id={id}
-        type={type}
-        ref={inputRef}
-        onChange={(e) => handleChange(e)}
-      />
+    <div className={styles.container}>
+      <div
+        className={`${styles.div} ${active ? styles.active : ''}`}
+        onClick={handleClick}
+      >
+        <input
+          className={styles.input}
+          id={id}
+          type={type}
+          ref={inputRef}
+          onChange={(e) => handleChange(e)}
+          onBlur={(e) => handleBlur(e)}
+        />
+        <div className={styles.wrapper}>
+          <label className={styles.label} htmlFor={id}>
+            {label}
+          </label>
+          {hasError && (
+            <div className={styles.errIcon}>
+              <WarnIcon />
+            </div>
+          )}
+        </div>
+      </div>
+      {hasError && <span className={styles.errMessage}>{error?.message}</span>}
     </div>
   );
 }
